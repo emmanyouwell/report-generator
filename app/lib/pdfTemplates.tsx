3972cf7b7bcd3de78d67713dd6b016474a302d9c
emmanyouwell <emingala02@gmail.com>
2025-07-06 00:04:03 +0800

    Updated prop types in pdfTemplate.tsx

--- app/lib/pdfTemplates.tsx
+++ app/lib/pdfTemplates.tsx
@@ -2,11 +2,18 @@
 import { ReactElement } from 'react';
 import Format1 from '@/app/ui/Format1';
 import Format2 from '@/app/ui/Format2';
-
+interface ContentBlock {
+  content: string;
+  img?: string;
+}
+interface PageData {
+  dateRange: string;
+  contents: ContentBlock[];
+}
 export function renderFormat1(): ReactElement {
   return <Format1 />;
 }
 
-export function renderFormat2(pages: any): ReactElement {
+export function renderFormat2(pages: PageData[]): ReactElement {
   return <Format2 pages={pages} />;
 }
