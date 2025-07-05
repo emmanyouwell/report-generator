4fc85e738eedd94b0d9629b93b9bd7d78e60a917
emmanyouwell <emingala02@gmail.com>
2025-07-06 00:17:57 +0800

    Fixed pdfTemplate.tsx

--- app/lib/pdfTemplates.tsx
+++ app/lib/pdfTemplates.tsx
@@ -2,6 +2,7 @@
 import { ReactElement } from 'react';
 import Format1 from '@/app/ui/Format1';
 import Format2 from '@/app/ui/Format2';
+import { DocumentProps } from '@react-pdf/renderer';
 interface ContentBlock {
   content: string;
   img?: string;
@@ -10,10 +11,10 @@ interface PageData {
   dateRange: string;
   contents: ContentBlock[];
 }
-export function renderFormat1(): ReactElement {
+export function renderFormat1(): ReactElement<DocumentProps> {
   return <Format1 />;
 }
 
-export function renderFormat2(pages: PageData[]): ReactElement {
+export function renderFormat2(pages: PageData[]): ReactElement<DocumentProps> {
   return <Format2 pages={pages} />;
 }
