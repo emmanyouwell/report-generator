8ea6f90d1efd982d5e5ce2c6d2dacb9bab359ded
emmanyouwell <emingala02@gmail.com>
2025-07-06 11:16:05 +0800

    Updated ApiUrl

--- app/ui/PdfViewer.tsx
+++ app/ui/PdfViewer.tsx
@@ -3,12 +3,16 @@ import {Viewer, Worker} from '@react-pdf-viewer/core';
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import {defaultLayoutPlugin} from '@react-pdf-viewer/default-layout'
+const apiUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : process.env.NEXT_PUBLIC_URL;
 export default function PdfViewer() {
     const defaultLayoutPluginInstance = defaultLayoutPlugin();
   return (
     <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.js'>
       <div className="h-[80vh]">
-        <Viewer fileUrl='http://localhost:3000/api/generate-pdf' plugins={[defaultLayoutPluginInstance]} />
+        <Viewer fileUrl={`${apiUrl}/api/generate-pdf`} plugins={[defaultLayoutPluginInstance]} />
       </div>
     </Worker>
   );
